0ff95fe3089775a641fc3cdb7e92d1976f7bd194
BrunoBatistaSoares <124104827+BrunoBatistaSoares@users.noreply.github.com>
2023-04-10 10:01:57 -0300

    placeholder for DB

--- angularCrud/src/app/shared/resources/product.ts
+++ angularCrud/src/app/shared/resources/product.ts
@@ -1,18 +1,24 @@
 export interface Product {
+    id: number;
     name: string;
     price: number;
     categorie: string;
+    description: string;
 }
 
 export const PRODUCTS = [
     {
+        id: 1,
         name: 'pão francês',
         price: 2.50,
-        categorie: 'Pão'
+        categorie: 'Pão',
+        description: 'delicioso pão francês quentinho com a casca levemente crocante e o miolo leve e macio'
     },
     {
+        id: 2,
         name: 'pão doce',
         price: 3.50,
-        categorie: 'Pão'
+        categorie: 'Pão',
+        description: 'doce de coco com lascas integrais e massa robusta, perfeito'
     }
 ]
